7d372bc9f34d88e74535fffd2f90dd42bc1d12fb
Abhiraj Adhikary <abhirajadhikary06@gmail.com>
2025-04-19 18:22:19 +0530

    Added changes

--- warranty_platform/settings.py
+++ warranty_platform/settings.py
@@ -107,8 +107,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'core/static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / "core" / "static",
+]
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
