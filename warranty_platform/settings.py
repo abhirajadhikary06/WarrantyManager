ee08b1e16f078669bc72948420a87391109d75a9
Abhiraj Adhikary <abhirajadhikary06@gmail.com>
2025-04-19 19:32:03 +0530

    added updates

--- warranty_platform/settings.py
+++ warranty_platform/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-177xxzzp9^m^42tnnvi-*%johh+2sd=1zed@l0*w4e_%%=vn*)'
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['billvault.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
@@ -125,8 +125,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 
-OCR_SPACE_API_KEY = os.getenv('OCR_SPACE_API_KEY')
-GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+OCR_SPACE_API_KEY = K87547568588957
+GEMINI_API_KEY = AIzaSyAJ3uU_eH5sPsRkadJl2Fg1k6nEglQA34o
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
